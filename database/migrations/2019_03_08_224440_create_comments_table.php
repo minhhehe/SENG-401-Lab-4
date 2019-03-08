0796bce483dde396defa3a67927fc785a8ce01ee
R Lee <alpha.r.lee.dev@gmail.com>
2019-03-08 16:48:00 -0700

    [task1.1] Added fields to comment migration.

--- database/migrations/2019_03_08_224440_create_comments_table.php
+++ database/migrations/2019_03_08_224440_create_comments_table.php
@@ -15,6 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('comment', 1023);
             $table->timestamps();
         });
     }
