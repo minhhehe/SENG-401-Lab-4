4b85c0479534483cb484720a6e841542eeb55491
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-03-17 16:38:04 -0600

    Cleaning up comments

--- resources/views/homeSubscriber.blade.php
+++ resources/views/homeSubscriber.blade.php
@@ -1,28 +1,5 @@
 @extends('layouts.userhome')
 
-<!-- @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-                   Welcome Mr. {{$user->email}}(lol)
-
-                   Here are what you can do:
-
-                  <a href="/books"> View all your books </a>
-
-                  <a href="/authors"> View alllll the authors </a>
-
-
-                  <a href="/{{$user->id}}/books/">View all the books you have subscribed</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection -->
-
 @section('home_menu')
 <a href="/books"> View all your books </a>
 <a href="/authors"> View alllll the authors </a>
--- resources/views/homeVisitor.blade.php
+++ resources/views/homeVisitor.blade.php
@@ -1,24 +1,5 @@
 @extends('layouts.userhome')
 
-<!-- @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-                   Welcome Mr. Visitor
-
-                   Here are what you can do:
-
-                  <a href="/books"> View all books </a>
-
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection -->
-
 @section('home_menu')
   <a href="/books"> View all books </a>
 @endsection
