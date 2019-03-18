bf1ecdbacb49d1f06c72fc3f42f766e306eff4c3
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-03-18 08:16:11 -0600

    View all subscriptions polish

--- resources/views/subscriptions/index.blade.php
+++ resources/views/subscriptions/index.blade.php
@@ -1,22 +1,31 @@
-@extends('layouts.welcome')
+@extends('layouts.subpage')
+
+@section('page_title')
+  Subscription List
+@endsection
+
+@section('subtitle')
+  <h5>View all subscriptions</h5>
+@endsection
 
 @section('content')
-  <h1 class="title"> List of subscriptions </h1>
+  <nav class="navbar navbar-light bg-light">
 
     @foreach ($subs as $sub)
-    <form method="POST" action="/subscriptions/{{$sub->id}}">
-
-      {{ method_field('DELETE') }}
-      {{ csrf_field() }}
-    <div class = "field">
-
-        {{ $sub->email }} subscribes to {{ $sub->name }} | {{ $sub->isbn}}
-        <button type="submit" class="button" name="delete">Delete Subscription</button>
-
-    </div>
-    </form>
+    <ul class="navbar-nav">
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+          <ul class="navbar-nav">
+            <form method="POST" action="/subscriptions/{{$sub->id}}">
+              {{ method_field('DELETE') }}
+              {{ csrf_field() }}
+              <button type="submit" class="btn" name="delete">Delete Subscription</button>
+            </form>
+          <li><div class="v-divider"></div></li>
+          <li class="nav-link">{{ $sub->email }} | subscribes to | {{ $sub->name }} | {{ $sub->isbn}}</li>
+        </ul>
+      </nav>
+    </ul>
     @endforeach
-
-
+  </nav>
 
 @stop
