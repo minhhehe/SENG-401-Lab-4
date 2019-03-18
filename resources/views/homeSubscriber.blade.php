2d96388d8956907674090c3a9a982c79c4dc4489
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-03-17 19:11:04 -0600

    updating subscriber link text

--- resources/views/homeSubscriber.blade.php
+++ resources/views/homeSubscriber.blade.php
@@ -9,13 +9,13 @@
 <nav class="navbar navbar-light bg-light">
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" href="/books"> View all your books </a>
+      <a class="nav-link" href="/books"> View your books </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="/authors"> View alllll the authors </a>
+      <a class="nav-link" href="/authors"> View all authors </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="/{{$user->id}}/books/">View all the books you have subscribed</a>
+      <a class="nav-link" href="/{{$user->id}}/books/">View your subscriptions</a>
     </li>
   </ul>
 </nav>
