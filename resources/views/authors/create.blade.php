bee6f61a2e68181cad45b1a1cec71e7652b9bcb9
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-03-17 23:36:32 -0600

    Add author polish done

--- resources/views/authors/create.blade.php
+++ resources/views/authors/create.blade.php
@@ -1,24 +1,31 @@
-@extends('layouts.welcome')
+@extends('layouts.subpage')
 
-<style>
-  .container { border:2px solid #ccc; width:300px; height: 100px; overflow-y: scroll; }
-</style>
+@section('page_title')
+  Add New Author
+@endsection
+
+@section('subtitle')
+  Ensure all fields are complete
+@endsection
+
 @section('content')
-  <h1> Author Create Page </h1>
+
   <form action="/authors/" method="post">
     {{@csrf_field()}}
-
+    <br>
     <div class="field">
-      <label class="label">Author's Name</label>
-      <div class="control">
-        <input class="input" type="text" placeholder="Enter the author's name" name="name">
 
-      </div>
+    <div class="control">
+      <label class="label col-md-3">Author's Name:</label>
+      <input class="input" type="text" placeholder="Enter the author's name" name="name">
     </div>
+    </div>
+
+    <div class="h-divider"></div>
 
     <div>
       </br>
-      <button type="submit">Add a new author entry</button>
+      <button class="btn" type="submit">Add author to system</button>
     </div>
 
 </form>
