f5dc6fe83f87c4056bebb2a43524723f08711f15
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-03-17 22:15:44 -0600

    /users index view polish

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,15 +9,20 @@
 @endsection
 
 @section('content')
-  <div class="">
+  <nav class="navbar navbar-light bg-light">
+
   @foreach ($users as $user)
-    <article>
-        <a href="{{ action('UsersController@show', [$user->id])}}"> {{ $user->email}} </a>
-        <p>
-          <a href="/users/{{ $user->id }}/edit"> Edit this record </a>
-        </p>
-    </article>
+  <ul class="navbar-nav">
+      <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <ul class="navbar-nav">
+            <li><a class="nav-item" href="/users/{{ $user->id }}/edit"><button type="button" class="btn">Edit</button></a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ action('UsersController@show', [$user->id])}}"> {{ $user->email}} </a></li>
+        </ul>
+      </nav>
+  </ul>
+  <div class="h-divider"></div>
   @endforeach
-  </div>
+
+  </nav>
 
 @stop
