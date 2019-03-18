7913fa400aa7075d69924b5253311405fde54fa2
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-03-18 11:01:08 -0600

    add subscription polish

--- resources/views/subscriptions/create.blade.php
+++ resources/views/subscriptions/create.blade.php
@@ -1,19 +1,32 @@
-@extends('layouts.welcome')
+@extends('layouts.subpage')
+
+@section('page_title')
+  Create Subscription
+@endsection
+
+@section('subtitle')
+  <h5>Add a subscription to a user</h5>
+@endsection
 
 @section('content')
-  <h1>Subscribe a user to a book</h1>
-
-  <hr/>
+<br>
 
   <form method="POST" action="/subscriptions">
     {{ csrf_field() }}
-    <div>
+    <div class="field">
+      <div class="control">
+      <label class="label col-md-2">User: </label>
       <select name="user_id">
         @foreach ($users as $user)
           <option value="{{$user->id}}"> {{$user->email}} </option>
         @endforeach
       </select>
+      </div>
+    </div>
 
+    <div class="field">
+      <div class="control">
+      <label class="label col-md-3">Book: </label><br>
       <select name="book_id">
         @foreach ($books as $book)
           <option value="{{$book->id}}"> {{$book->name}} | {{ $book->isbn }} </option>
@@ -21,8 +34,10 @@
       </select>
     </div>
 
+    <div class="h-divider"></div>
+
     <div>
-      <button type="submit">Subscribe</button>
+      <button class="btn" type="submit">Subscribe</button>
     </div>
 
     @include('error')
