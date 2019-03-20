78c9805435fcbb182e19ca3926c6e2fd540b2cd6
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-03-20 11:08:40 -0600

    Book Index Polish complete

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,10 +1,44 @@
-@extends('layouts.welcome')
+@extends('layouts.subpage')
+
+@section('page_title')
+  System Books
+@endsection
+
+@section('subtitle')
+  @if ($role == 'admin')
+    <a href="/books/create"><button type="button" class="btn"> Add a new book</button> </a>
+  @endif
+@endsection
 
 @section('content')
-  <h1> Books Page </h1>
+  <nav class="navbar navbar-light bg-light">
+
+  @foreach ($books as $book)
+  <ul class="navbar-nav">
+      <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="{{ action('BooksController@show', [$book->id])}}"> {{ $book->name}} </a></li>
+        </ul>
+        </nav>
+      <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        @if ($role == 'admin')
+          <li><a class="nav-item" href="/books/{{ $book->id }}/edit"><button type="button" class="btn">Edit</button> </a>
+          <li><div class="v-divider"></div></li>
+          <form action="/books/{{ $book->id }}" method="post">
+            {{ @csrf_field() }}
+            {{ @method_field('DELETE') }}
+            <button type="submit" class="btn" name="deleteButton">Delete</button>
+          </form>
+          <li><div class="v-divider"></div></li>
+        @endif
+      </ul>
+    </nav>
+  </ul>
+  <div class="h-divider"></div>
+  @endforeach
 
 
-  <div class="container_big">
+  <!-- <div class="container_big">
   @foreach ($books as $book)
     <article>
         <a href="{{ action('BooksController@show', [$book->id])}}"> {{ $book->name}} </a>
@@ -20,13 +54,6 @@
         @endif
     </article>
   @endforeach
-  </div>
-
-
-  @if ($role == 'admin')
-  <p>
-    <a href="/books/create"> Create a new one </a>
-  </p>
-  @endif
+  </div> -->
 
 @stop
