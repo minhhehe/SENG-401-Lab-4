b4304e91066e2bd20c07900a8905bfd15d8db996
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-03-20 11:15:13 -0600

    Cleaning up comments

--- resources/views/users/subscribedBooks.blade.php
+++ resources/views/users/subscribedBooks.blade.php
@@ -24,22 +24,4 @@
 <div class="h-divider"></div>
 @endforeach
 
-<!-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-                   Welcome Mr. {{$user->email}}(lol)
-
-                   Here are books you have been subscribed to:
-
-
-                  @foreach ($books as $book)
-                    <a href='/books/{{$book->id}}'> {{$book->name}} </a>
-                  @endforeach
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
 @endsection
