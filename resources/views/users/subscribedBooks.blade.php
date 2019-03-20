6c1f5b7a1bf25a1fbad0e78c88c0990eb3e8c61c
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-03-20 11:14:49 -0600

    View your subscriptions page polish

--- resources/views/users/subscribedBooks.blade.php
+++ resources/views/users/subscribedBooks.blade.php
@@ -1,7 +1,30 @@
-@extends('layouts.app')
+@extends('layouts.subpage')
+
+@section('page_title')
+  Your Subscriptions
+@endsection
+
+@section('subtitle')
+  Subscription list for user: {{$user->email}}
+@endsection
 
 @section('content')
-<div class="container">
+<nav class="navbar navbar-light bg-light">
+
+@foreach ($books as $book)
+<ul class="navbar-nav">
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+      <ul class="navbar-nav">
+
+          <a class="nav-link" href='/books/{{$book->id}}'> {{$book->name}} </a>
+
+    </ul>
+  </nav>
+</ul>
+<div class="h-divider"></div>
+@endforeach
+
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -18,5 +41,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
