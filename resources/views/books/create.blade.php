8eeee0be0ce59e8c48d41d9abfe717e3bd34760d
Jesse Fowler <jesse.fowler1@ucalgary.ca>
2019-03-20 11:28:04 -0600

    Create book page polish

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -1,57 +1,76 @@
-@extends('layouts.welcome')
+@extends('layouts.subpage')
 
-<style>
-  .container { border:2px solid #ccc; width:300px; height: 100px; overflow-y: scroll; }
-</style>
+@section('page_title')
+  Add a New Book
+@endsection
+
+@section('subtitle')
+  Book Information
+@endsection
+
 @section('content')
-  <h1> Book Edit Page </h1>
   <form action="/books/" method="post">
     {{@csrf_field()}}
-
+    <br>
     <div class="field">
-      <label class="label">Name</label>
       <div class="control">
-        <input class="input" type="text" placeholder="Enter the book's name" name="name">
+        <label class="label col-md-3">Name</label>
+        <input class="input" type="text" placeholder="Enter the book's title" name="name">
       </div>
     </div>
 
+    <div class="h-divider"></div>
+
     <div class="field">
-      <label class="label">ISBN</label>
       <div class="control">
+        <label class="label col-md-3">ISBN</label>
         <input class="input" type="text" placeholder="Enter the ISBN" name="isbn">
       </div>
     </div>
 
+    <div class="h-divider"></div>
+
     <div class="field">
-      <label class="label">Year</label>
       <div class="control">
+        <label class="label col-md-3">Year</label>
         <input class="input" type="text" placeholder="Enter the year" name="year">
       </div>
     </div>
 
     <div class="field">
-      <label class="label">Publisher</label>
       <div class="control">
+        <label class="label col-md-3">Publisher</label>
         <input class="input" type="text" placeholder="Enter the publisher" name="publisher">
       </div>
     </div>
 
+    <div class="h-divider"></div>
+
     <div class="field">
-      <label class="label">Image</label>
       <div class="control">
+        <label class="label col-md-3">Image</label>
         <input class="input" type="text" placeholder="Enter the image url" name="image">
       </div>
     </div>
 
-    <div class="container_small">
-      @foreach ($authors as $author)
-        <input type="checkbox" name="authors[]" value="{{$author->id}}"> <label class="label">{{$author->name}}</label>
-        <br />
-      @endforeach
+    <div class="h-divider"></div>
+
+    <div class="field">
+      <div class="control">
+        <label class="label col-md-3" style="vertical-align: top;">Author(s)</label>
+        <div class="container_small" style="display:inline-block;">
+          @foreach ($authors as $author)
+            <input type="checkbox" name="authors[]" value="{{$author->id}}"> <label class="label">{{$author->name}}</label>
+            <br />
+          @endforeach
+        </div>
+      </div>
     </div>
 
+    <div class="h-divider"></div>
+
     <div>
-      <button type="submit">Create a new book entry</button>
+      <button class="btn" type="submit">Create a new book entry</button>
     </div>
 
   </form>
